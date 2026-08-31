6b6f3b081c456a910cebf556e2185bdfb29b398e
Alondra Obregon <alondra.obregon@utec.edu.pe>
2026-08-31 13:45:08 -0500

    Implementación de push en queue

--- Pilas_y_Colas/queue.cpp
+++ Pilas_y_Colas/queue.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace ::std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+template<typename T>
+struct Node {
+    T data;
+    Node* next;
+};
+
+template<typename T>
+class Queue {
+private:
+    Node<T>* front;
+    Node<T>* back;
+    int count;
+public:
+    Queue() : front(nullptr), back(nullptr), count(0){}
+
+    void push(T value) {
+        Node<T>* newNode = new Node<T>{value, nullptr};
+        if (front == nullptr and back == nullptr) {
+            front = back = newNode;
+        }
+        else {
+            back -> next = newNode;
+            back = newNode;
+        }
+        count++;
+    }
+};
